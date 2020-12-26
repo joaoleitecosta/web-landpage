443aeb46d82b8c8cd71c8b4ad260dd8e1241c831
Joao Leite <joao_leite@hotmail.com.br>
2020-12-26 20:30:20 -0300

    refactor: alteracao no nome do desenvolvedor

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,9 +8,7 @@ const Footer = () => (
   <S.Wrapper>
     <Container>
       <p>
-        Desenvolvido por{' '}
-        <a href="https://willianjusten.com.br/">Willian Justen </a>e
-        <a href="https://guilouro.dev"> Guilherme Louro</a>
+        Desenvolvido por <a href="#">João Leite </a>e
       </p>
       <p>
         Design por <a href="https://dribbble.com/vmarcosp">Marcos Oliveira</a>
